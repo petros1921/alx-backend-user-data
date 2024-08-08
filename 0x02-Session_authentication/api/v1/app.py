f918de6b8cedbc9ff01742e8b7400263670804b1
Petros Worku <31696803-petrosworku19@users.noreply.replit.com>
2024-08-08 19:45:17 +0000

    all

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -65,8 +65,7 @@ def authenticate_user():
         ]
         if auth.require_auth(request.path, excluded_paths):
             user = auth.current_user(request)
-            if auth.authorization_header(request) is None and \
-                    auth.session_cookie(request) is None:
+            if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
                 abort(401)
             if user is None:
                 abort(403)
